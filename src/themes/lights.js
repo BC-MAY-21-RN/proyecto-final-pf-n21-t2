020a8921b4a1b6186dbdc1faa4012a23a6dbd95c
Hilarión <50677192+Darkboy5120@users.noreply.github.com>
2022-01-25 16:17:53 -0600

    light theme
    default theme's file
    
    Co-Authored-By: thisisofficial <thisisofficiallyofficial@gmail.com>
    Co-Authored-By: Luis Fernando Gómez Hinojosa <luis.gh8907@gmail.com>
    Co-Authored-By: Manuel Rios <53833843+xManuel007@users.noreply.github.com>
    Co-Authored-By: Hugo Alvarez <42789412+Hugoneytor@users.noreply.github.com>

---
+++ src/themes/lights.js
@@ -0,0 +1,35 @@
+const palette = {
+  black: '#000000',
+  white: '#ffffff',
+  purple: '#5C33F6',
+  lightpurple: '#A239EA',
+  pink: '#FF94CC',
+  lightpink: '#F0D9E7',
+};
+
+const theme = {
+  color: {
+    primary1: palette.purple,
+    primary2: palette.lightpurple,
+    primary3: palette.pink,
+    primary4: palette.lightpink,
+    secondary1: palette.white,
+    secondary2: palette.black,
+  },
+  font: {
+    xs: 12,
+    m: 16,
+    xl: 20,
+    xxl: 24,
+    xxxl: 28,
+  },
+  spacing: {
+    xs: 4,
+    m: 8,
+    xl: 12,
+    xxl: 16,
+    xxxl: 20,
+  },
+};
+
+export default theme;
