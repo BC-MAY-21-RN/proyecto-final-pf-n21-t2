61b10bd415553fdb3e77345b1f2e2266f5fa9a56
Hilarión <50677192+Darkboy5120@users.noreply.github.com>
2022-01-25 21:12:17 -0600

    fixed firestore erros
    - the app name in manifest was wrong, used to be foodog, now its foo
    - reinstalled the whole app to correctly test auth services, everything its ok

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -5,24 +5,22 @@ import CustomInput from "../CustomInput";
 import styles from './styles'
 import auth from '@react-native-firebase/auth';
 
-//auth.signInWithEmailAndPassword('luis@sincorreo.com', 'password');
+auth().signInWithEmailAndPassword('luis@sincorreo.com', 'password');
 
 const LoginForm = () => {
-  // useEffect(() => {
-  //   const subscriber = auth().onAuthStateChanged(user => {
-  //     console.log(user);
-  //   });
-  //   return subscriber;
-  // }, []);
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(user => {
+      if (user) {
+        console.log("user signed in");
+      }
+    });
+    return subscriber;
+  }, []);
 
   return (
     <View style={styles.container}>
-
-
       <CustomInput title="Email" />
-
       <CustomInput title="Password" />
-
       <CustomButton width={150} title="Sign in" onPress={() => console.log("sign in")} />
 
     </View>
