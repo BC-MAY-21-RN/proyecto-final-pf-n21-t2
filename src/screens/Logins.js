397a8004e7768b219ab70dc4a803876144ce7802
Hilarión <50677192+Darkboy5120@users.noreply.github.com>
2022-01-31 18:51:34 -0600

    codeclimate duplication fix

--- src/screens/Logins.js
+++ src/screens/Logins.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import {View} from 'react-native';
 import LoginForm from '../components/LoginForm';
-import ToNextSectionText from '../components/ToNextSectionText';
-import ImageLogo from '../components/ImageLogo';
+import SignWithLogo from '../components/SignWithLogo';
 
 const Login = ({navigation}) => {
-  return(
-    <View style={{flex: 1}}>
-      <ImageLogo flex={3} />  
+  return (
+    <SignWithLogo navigation={navigation} imageFlex={3} footer={{text: `Don't have an account yet? `, nextSection: {label: 'SignUp', section: 'SignUpType'}}}>
       <LoginForm navigation={navigation} />
-      <ToNextSectionText navigation={navigation} text="Don't have an account yet? " nextSection={{label: 'SignUp', section: 'SignUpType'}} />
-    </View>
+    </SignWithLogo>
   )
 }
 
