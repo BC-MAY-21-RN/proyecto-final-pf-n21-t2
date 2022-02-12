50cabbb7f7a490a3346771dd9c56bc2251b22698
Manuel Eduardo Rios Martinez <maanuu.009@gmail.com>
2022-02-11 18:07:36 -0600

    Make The generic image

--- src/screens/HomeWalker.js
+++ src/screens/HomeWalker.js
@@ -3,10 +3,10 @@ import React from 'react';
 import CardGeneric from '../components/GenericCard';
 
 const DATA = [
-  {id: 1, name: "Bruno diaz", duration: 65, start: 70},
-  {id: 2, name: "foo", duration: 2, start: 2},
-  {id: 3, name: "foo", duration: 2, start: 2},
-  {id: 4, name: "foo", duration: 2, start: 2}
+  {id: 1, name: "Bruno diaz", duration: 65, start: 70, ImageUri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFrCAT0yddSLR1q64m5s2bLUkMgFUaFamivJ5E3pUVWX_AY6fOgfJHyDP9haXSHB5WgjE&usqp=CAU'},
+  {id: 2, name: "Sandalia", duration: 2, start: 2, ImageUri: 'https://assets.mycast.io/actor_images/actor-zendaya-17981_large.jpg?1578258382'},
+  {id: 3, name: "Luisito Comunica", duration: 2, start: 2, ImageUri: 'https://strikers.futbol/__export/1623776623054/sites/strikers/img/2021/06/15/programa_en_memoria_a_josx_rafael_albrecht_-_2021-06-15t140243_113.png_1329611142.png'},
+  {id: 4, name: "Amlo", duration: 2, start: 2, ImageUri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGuLMh3vuYmMC5mbBnuNpi0XOApwr3vnHfsw&usqp=CAU'}
 ];
 
 const HomeWalker = ({navigation}) => {
@@ -15,7 +15,9 @@ const HomeWalker = ({navigation}) => {
       navigation={navigation}
       Name={item.name}
       Duration={item.duration}
-      Start={item.start} />
+      Start={item.start}
+      ImageUri={item.ImageUri}
+      />
   );
   
   return (
--- src/screens/WalkerDetailsClient.js
+++ src/screens/WalkerDetailsClient.js
@@ -1,85 +1,23 @@
 import { StyleSheet, Text, View, Image } from 'react-native';
 import React from 'react';
-import CustomButton from '../components/CustomButton';
+import CustomImage from '../components/CustomImage';
 
-const WalkerDetailsClient = ({route}) => {
+const WalkerDetailsClient = () => {
 
-  const {Name, Duration, Start} = route.params;
-
-  const buttonWidth = 300;
-  const bottomMargin = {marginBottom: 20};
-
+  const testdata = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFrCAT0yddSLR1q64m5s2bLUkMgFUaFamivJ5E3pUVWX_AY6fOgfJHyDP9haXSHB5WgjE&usqp=CAU';
   return (
-    <View style={styles.container}>
-      <View style={styles.owner}>
-        <Image style={styles.image}
-          source={{
-            uri: 'https://i.ytimg.com/vi/XfRPlBP7f2E/maxresdefault.jpg',
-          }}
-
-        />
-        <View style={styles.info}>
-          <Text style={styles.info1}>Dueño</Text>
-          <Text style={styles.info2}>{Name}</Text>
-          <Text style={styles.info3}>Paseo de {Duration} minutos</Text>
-          <Text style={styles.info3}>Comienza en {Start} minutos</Text>
-        </View>
-      </View>
-      <View style={styles.dogData}>
-        {/* Datos del Perro */}
-      </View>
-      <View style={styles.buttons}>
-        <CustomButton style={bottomMargin} title='Aceptar' width={buttonWidth} />
-        <CustomButton  style={bottomMargin} title='Cancelar' width={buttonWidth} />
-      </View>
+    <View>
+      <Text>detail's client Screen</Text>
+        <CustomImage ImageUri={testdata}/>
     </View>
   );
 };
 
 export default WalkerDetailsClient;
-                                              
-const styles = StyleSheet.create({
-  container:{
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center'
-  },
-  owner:{
-    flexDirection: 'row',
-    marginTop: 30,
-  },
-  image: {
-    width: 100,
-    height: 100,
-    borderRadius: 60,
-    marginRight: 20
-  },
-  info:{
-    marginBottom: 30,
-  },
-  info1:{
-    fontSize: 17,
-    color: 'gray'
-  },
-  info2:{
-    color: 'black',
-    fontSize: 22,
-    fontWeight: 'bold',
-    marginBottom: 10
-  },
-  info3:{
-    color: 'black',
-    
-  },
-  dogData:{
-    width: 300,
-    height: 200,
-    borderWidth: 5,
-    borderRadius: 40,
-    borderColor: '#A239EA',
-    marginBottom: 30
-  },
-  buttons:{
 
+const styles = StyleSheet.create({
+  test: {
+    width: 200,
+    height: 200
   }
 });
