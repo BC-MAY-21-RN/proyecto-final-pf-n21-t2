8b9c6003520ce1974600da0025fee1f2ac519837
Hilarión <50677192+Darkboy5120@users.noreply.github.com>
2022-02-16 18:41:31 -0600

    imagepicker component
    - new component to get image value

--- src/components/CustomButton/index.js
+++ src/components/CustomButton/index.js
@@ -5,12 +5,13 @@ import styles from './styles'
 
 import Icon from 'react-native-vector-icons/Ionicons'
 
-const CustomButton = ({ marginBottom, marginTop, style, loading, disabled, title, onPress, width, leftIconName }) => {
+const CustomButton = ({ color, marginBottom, marginTop, style, loading, disabled, title, onPress, width, leftIconName }) => {
   const iconWithTextStyle = title ? styles.iconWithText : null
   const leftIcon = leftIconName ? <Icon style={[iconWithTextStyle, styles.icon, styles.textColor]} name={leftIconName} size={24} /> : null
+  const colorStyle = color ?? { backgroundColor: theme.color.primary2 }
   return (
     <View style={[styles.container, style, { marginTop, marginBottom }]}>
-      <Pressable style={[{ width: width }, styles.button, disabled ? styles.disabled : null]} onPress={onPress}>
+      <Pressable style={[{ width: width }, styles.button, colorStyle, disabled ? styles.disabled : null]} onPress={onPress}>
         {leftIcon}
         {loading ? <ActivityIndicator size="small" color={theme.color.secondary1} /> : <Text style={[styles.text, styles.textColor]}>{title}</Text>}
       </Pressable>
--- src/screens/AddPet.js
+++ src/screens/AddPet.js
@@ -1,11 +1,18 @@
-import { Text, StyleSheet } from 'react-native'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import GenericContainer from '../containers/GenericContainer'
 import { CustomInput } from '../components/CustomInput'
+import ImageInput from '../components/ImageInput'
 import theme from '../themes/lights'
 import CustomButton from '../components/CustomButton'
 
 const AddPet = () => {
+  const [value, setValue] = useState()
+  const [ok, setOk] = useState()
+
+  useEffect(() => {
+    console.log(value)
+  }, [value])
+
   return (
     <GenericContainer scroll={true}>
       <CustomInput title={'Pet Name'}/>
@@ -13,20 +20,10 @@ const AddPet = () => {
       <CustomInput title={'Weight'} />
       <CustomInput title={'Height'} />
       <CustomInput height={100} title={'Special Cares'} />
-      <Text style={styles.text}>Select or Take a Photo of your pet</Text>
-      <CustomButton leftIconName="cloud-upload-outline" />
+      <ImageInput title="imagen" {...{ value, setValue, setOk }} />
       <CustomButton width={150} marginTop={theme.spacing.xl} title={'ADD'} />
     </GenericContainer>
   )
 }
-
-const styles = StyleSheet.create({
-  text: {
-    color: 'black',
-    fontSize: 15,
-    fontWeight: 'bold',
-    marginBottom: 15
-  }
-})
 
 export default AddPet
