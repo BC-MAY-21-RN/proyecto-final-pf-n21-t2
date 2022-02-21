43dc3435eea4ebcee79ee9a623d106117c7cddc8
Hilarión <50677192+Darkboy5120@users.noreply.github.com>
2022-02-21 17:00:24 -0600

    fixed buttons problems while getting disable prop

--- src/components/ButtonSign/index.js
+++ src/components/ButtonSign/index.js
@@ -5,7 +5,7 @@ import theme from '../../themes/lights'
 const ButtonSign = ({ title, onPress, ok, loading }) => {
   ok = ok ?? true
   return (
-    <CustomButton width={200} marginTop={theme.spacing.xxl} disabled={!ok} loading={loading} title={title} onPress={onPress} />
+    <CustomButton width={200} marginTop={theme.spacing.xxl} ok={ok} loading={loading} title={title} onPress={onPress} />
   )
 }
 
