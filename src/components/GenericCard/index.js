53729afaed6c64875899b8ba69af1c4891706ec6
Hilarión <50677192+Darkboy5120@users.noreply.github.com>
2022-03-02 18:18:04 -0600

    added walkers walkings' screen

--- src/components/GenericCard/index.js
+++ src/components/GenericCard/index.js
@@ -1,27 +1,56 @@
-import { Text, View, TouchableOpacity, ScrollView } from 'react-native'
+import { Text, View, TouchableOpacity } from 'react-native'
 import React from 'react'
 import styles from './styles'
 import CustomImage from '../CustomImage'
 
-const CardGeneric = ({ navigation, Name, Duration, Start, ImageUri }) => {
+const getHumanValue = value => {
+  let result = ''
+  const seconds = 1000
+  const minutes = seconds * 60
+  const hours = minutes * 60
+  const days = hours * 24
+  if (value < 1000) {
+    result = 'just now'
+  } if (value / seconds > 1) {
+    result = `${Math.floor(value / seconds)} seconds`
+  } if (value / minutes > 1) {
+    result = `${Math.floor(value / minutes)} minutes`
+  } if (value / hours > 1) {
+    result = `${Math.floor(value / hours)} hours`
+  } if (value / days > 1) {
+    result = `${Math.floor(value / days)} days`
+  }
+  return result
+}
+
+const getTravelValues = (startDatetime, endDatetime) => {
+  const currentDate = new Date()
+  const rawDuration = currentDate.getTime() - parseInt(startDatetime)
+  const humanDuration = getHumanValue(rawDuration)
+  const rawStartsIn = parseInt(endDatetime) - parseInt(startDatetime)
+  const humanStartsIn = getHumanValue(rawStartsIn)
+  return [humanDuration, humanStartsIn]
+}
+
+const CardGeneric = ({ navigation, Name, Duration, Start, ImageUri, startDatetime, endDatetime }) => {
   const onPress = () => {
     navigation.navigate('WalkerDetailsClient', {
       Name, Duration, Start, ImageUri
     })
   }
 
+  const [travelDuration, travelStartsIn] = getTravelValues(startDatetime, endDatetime)
+
   return (
-    <ScrollView style={styles.Space}>
-      <TouchableOpacity style={styles.CardBox} onPress={onPress}>
-        <CustomImage ImageUri={ImageUri}/>
-        <View style={styles.Data}>
-          <Text style={styles.Tittle}>Owner</Text>
-          <Text>{Name}</Text>
-          <Text>The travel has a duration of 8hr</Text>
-          <Text>The travel starts in 1hr</Text>
-        </View>
-      </TouchableOpacity>
-    </ScrollView>
+    <TouchableOpacity style={styles.CardBox} onPress={onPress}>
+      <CustomImage ImageUri={ImageUri}/>
+      <View style={styles.Data}>
+        <Text style={styles.Tittle}>Owner</Text>
+        <Text>{Name}</Text>
+        <Text>Duration of {travelDuration}</Text>
+        <Text>Starts in {travelStartsIn}</Text>
+      </View>
+    </TouchableOpacity>
   )
 }
 
--- src/screens/HomeWalker.js
+++ src/screens/HomeWalker.js
@@ -1,41 +1,56 @@
-import { View, FlatList, StyleSheet } from 'react-native'
 import React from 'react'
 import CardGeneric from '../components/GenericCard'
+import CustomFlatList from '../components/CustomFlatList'
+import GenericContainer from '../containers/GenericContainer'
+import fbShortcuts from '../assets/controllers/firebaseShortcuts'
+import { userSession } from '../store/reducers/userSession'
 
-const DATA = [
-  { id: 1, name: 'Bruno diaz', duration: 65, start: 70, ImageUri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFrCAT0yddSLR1q64m5s2bLUkMgFUaFamivJ5E3pUVWX_AY6fOgfJHyDP9haXSHB5WgjE&usqp=CAU' },
-  { id: 2, name: 'Sandalia', duration: 2, start: 2, ImageUri: 'https://assets.mycast.io/actor_images/actor-zendaya-17981_large.jpg?1578258382' },
-  { id: 3, name: 'Luisito Comunica', duration: 2, start: 2, ImageUri: 'https://strikers.futbol/__export/1623776623054/sites/strikers/img/2021/06/15/programa_en_memoria_a_josx_rafael_albrecht_-_2021-06-15t140243_113.png_1329611142.png' },
-  { id: 4, name: 'Amlo', duration: 2, start: 2, ImageUri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGuLMh3vuYmMC5mbBnuNpi0XOApwr3vnHfsw&usqp=CAU' }
-]
+const getClients = setClients => {
+  const walkerId = userSession.getState().id
+  fbShortcuts.getCollection('Walkings').where('walkerId', '==', walkerId).get()
+    .then(async (querySnapshot) => {
+      const walkings = []
+      await new Promise(resolve => {
+        const getClients = []
+        querySnapshot.forEach(async (snapshot) => {
+          const row = snapshot.data()
+          row.id = snapshot.id
+          const user = fbShortcuts.getCollection('Users').where('useruid', '==', row.clientId).get()
+          getClients.push(user)
+          walkings.push(row)
+        })
+        Promise.all(getClients).then(data => {resolve(data)})
+      }).then(data => {
+        data.forEach((client, index) => {
+          client.forEach(clientData => {
+            const clientRow = clientData.data()
+            walkings[index].image = fbShortcuts.getImage(`Users%2F${clientRow.useruid}%2F${clientRow.imageName}`)
+            walkings[index].username = clientRow.username
+          })
+        })
+        setClients(walkings)
+      })
+    })
+}
 
 const HomeWalker = ({ navigation }) => {
   const renderItem = ({ item }) => (
     <CardGeneric
       navigation={navigation}
-      Name={item.name}
+      Name={item.username}
       Duration={item.duration}
       Start={item.start}
-      ImageUri={item.ImageUri}
+      ImageUri={item.image}
+      startDatetime={item.startDatetime}
+      endDatetime={item.endDatetime}
       />
   )
 
   return (
-    <View style={styles.whiteBack}>
-      <FlatList
-        data={DATA}
-        renderItem={renderItem}
-        keyExtractor={item => item.id}
-      />
-    </View>
+    <GenericContainer>
+      <CustomFlatList render={renderItem} get={getClients} empty="No pending services" />
+    </GenericContainer>
   )
 }
-
-const styles = StyleSheet.create({
-  whiteBack: {
-    backgroundColor: 'white',
-    flex: 1
-  }
-})
 
 export default HomeWalker
