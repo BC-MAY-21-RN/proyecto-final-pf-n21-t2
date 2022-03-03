be381e7855a38e99faaa6cf324fb069d766cc75a
Hugoneytor <halvarez0@ucol.mx>
2022-03-03 17:50:41 -0600

    Reviews submitted to Firebase We finished to implement the add review functionality to add in the firestore database
    Co-authored-by: Manuel Rios <xManuel007@users.noreply.github.com>
    Co-authored-by: Luis Fernando Gómez Hinojosa <luis.gh8907@gmail.com>

---
+++ src/hooks/useAddReview.js
@@ -0,0 +1,13 @@
+import useInput from './useInput'
+import inputValidations from '../assets/datasets/inputValidations'
+import useSubmit from './useSubmit'
+import FormHookPipe from '../assets/controllers/FormHookPipe'
+
+const useAddReview = () => {
+  return FormHookPipe({
+    review: useInput(inputValidations.string),
+    submit: useSubmit()
+  })
+}
+
+export default useAddReview
--- src/screens/AddReview.js
+++ src/screens/AddReview.js
@@ -1,29 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CustomInput from '../components/CustomInput'
 import GenericContainer from '../containers/GenericContainer'
 import CustomButton from '../components/CustomButton'
 import { AirbnbRating } from 'react-native-ratings'
 import UserPresentation from '../components/UserPresentation'
+import { firebase } from '@react-native-firebase/firestore'
+import { userSession } from '../store/reducers/userSession'
+import useAddReview from '../hooks/useAddReview'
 
 const AddReview = ({ navigation, route }) => {
+  const form = useAddReview()
+
+  const [ratings, setRatings] = useState(0)
+
+  const getFormResult = form => {
+    return {
+      review: form.review.value,
+      ratings: ratings,
+      useruid: userSession.getState().id,
+      walkeruid: route.params.id
+    }
+  }
+
   const { image, name } = route.params
-  /* const { image } = route.params */
+  const handleAddReview = () => {
+    firebase.firestore().collection('Reviews').add(getFormResult(form)).then(() => navigation.goBack())
+  }
 
   return (
     <GenericContainer>
       <UserPresentation name={name} image={{ uri: image }} />
-      <AirbnbRating/>
-      <CustomInput placeholder={'Add your review here...'} />
-      <CustomButton title={'Save Review'} borderRadius={15} onPress={() => { alert('New review added :D') } }/>
+      <AirbnbRating onFinishRating={(rating) => setRatings(rating)} defaultRating={5}/>
+      <CustomInput placeholder={'Add your review here...'} {...form.review}/>
+      <CustomButton title={'Save Review'} borderRadius={15} onPress={ handleAddReview }/>
     </GenericContainer>
   )
 }
 
 export default AddReview
-
-/* const styles = StyleSheet.create({
-  img: {
-    alignSelf: 'center'
-  }
-})
- */
