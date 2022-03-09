4c2ae848deed7818943e61dc19c451f7de444ae3
Hilarión <50677192+Darkboy5120@users.noreply.github.com>
2022-03-08 18:05:43 -0600

    connected review walker when client finish walking

--- src/screens/AddReview.js
+++ src/screens/AddReview.js
@@ -7,10 +7,18 @@ import UserPresentation from '../components/UserPresentation'
 import { firebase } from '@react-native-firebase/firestore'
 import { userSession } from '../store/reducers/userSession'
 import useAddReview from '../hooks/useAddReview'
+import theme from '../themes/lights'
+
+const resetNavigation = navigation => {
+  navigation.reset({
+    index: 0,
+    routes: [{ name: 'Client', params: { useruid: userSession.getState().id } }]
+  })
+}
 
 const AddReview = ({ navigation, route }) => {
   const form = useAddReview()
-
+  const [loading, setLoading] = useState(false)
   const [ratings, setRatings] = useState(0)
 
   const getFormResult = form => {
@@ -24,15 +32,23 @@ const AddReview = ({ navigation, route }) => {
 
   const { image, name } = route.params
   const handleAddReview = () => {
-    firebase.firestore().collection('Reviews').add(getFormResult(form)).then(() => navigation.goBack())
+    setLoading(true)
+    firebase.firestore().collection('Reviews').add(getFormResult(form)).then(() => {
+      setLoading(false)
+      resetNavigation(navigation)
+    })
+  }
+  const handleCancel = () => {
+    resetNavigation(navigation)
   }
 
   return (
-    <GenericContainer>
+    <GenericContainer scroll>
       <UserPresentation name={name} image={{ uri: image }} />
       <AirbnbRating onFinishRating={(rating) => setRatings(rating)} defaultRating={5}/>
       <CustomInput placeholder={'Add your review here...'} {...form.review}/>
-      <CustomButton title={'Save Review'} borderRadius={15} onPress={ handleAddReview }/>
+      <CustomButton title={'Save Review'} width={150} loading={loading} borderRadius={15} onPress={ handleAddReview }/>
+      <CustomButton title={'Cancel'} width={100} marginTop={theme.spacing.m} borderRadius={15} onPress={ handleCancel }/>
     </GenericContainer>
   )
 }
